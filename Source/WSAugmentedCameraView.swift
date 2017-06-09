0e37d68d7611ed5ecf2c3815a6aa20cdfbcd16e1
Ricardo Pereira <m@ricardopereira.eu>
2017-06-09 20:19:34 +0100

    Camera setup and Face detection

---
+++ Source/WSAugmentedCameraView.swift
@@ -0,0 +1,310 @@
+//
+//  WSAugmentedCameraView.swift
+//  WSAugmentedCamera
+//
+//  Created by Ricardo Pereira on 09/06/2017.
+//  Copyright © 2017 Whitesmith. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+public class WSAugmentedCameraView: UIView {
+
+    // MARK: - Session Management
+
+    private let session = AVCaptureSession()
+    // Communicate with the session and other session objects on this queue.
+    private let sessionQueue = DispatchQueue(label: "co.whitesmith.WSAugmentedCameraView.session", attributes: [], target: nil)
+
+
+    // MARK: - Inputs
+    private var deviceInput: AVCaptureDeviceInput!
+
+    // MARK: - Outputs
+    private let photoOutput = AVCapturePhotoOutput()
+    private let metadataOutput = AVCaptureMetadataOutput()
+    private let videoDataOutput = AVCaptureVideoDataOutput()
+
+
+    // MARK: - Initializers
+
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupGestures()
+    }
+
+    public required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+
+    // MARK: - UIView
+
+    override public class var layerClass: AnyClass {
+        return AVCaptureVideoPreviewLayer.self
+    }
+
+    fileprivate var videoPreviewLayer: AVCaptureVideoPreviewLayer {
+        guard let layer = layer as? AVCaptureVideoPreviewLayer else {
+            fatalError("Expected `AVCaptureVideoPreviewLayer` type for layer")
+        }
+        return layer
+    }
+
+    // MARK: - Setup
+
+    private enum SessionSetupResult {
+        case success
+        case notAuthorized
+        case configurationFailed
+    }
+
+    private var setupResult: SessionSetupResult = .success
+
+    private func setupSession() {
+        if setupResult != .success {
+            return
+        }
+
+        session.beginConfiguration()
+        defer {
+            session.commitConfiguration()
+        }
+
+        /*
+         We do not create an AVCaptureMovieFileOutput when setting up the session because the
+         AVCaptureMovieFileOutput does not support movie recording with AVCaptureSessionPresetPhoto.
+         */
+        session.sessionPreset = AVCaptureSessionPresetPhoto
+        videoPreviewLayer.session = session
+
+        // Add video input.
+        do {
+            var defaultVideoDevice: AVCaptureDevice?
+
+            // Choose the back dual camera if available, otherwise default to a wide angle camera.
+            if let dualCameraDevice = AVCaptureDevice.defaultDevice(withDeviceType: .builtInDualCamera, mediaType: AVMediaTypeVideo, position: .back) {
+                defaultVideoDevice = dualCameraDevice
+            } else if let backCameraDevice = AVCaptureDevice.defaultDevice(withDeviceType: .builtInWideAngleCamera, mediaType: AVMediaTypeVideo, position: .back) {
+                // If the back dual camera is not available, default to the back wide angle camera.
+                defaultVideoDevice = backCameraDevice
+            } else if let frontCameraDevice = AVCaptureDevice.defaultDevice(withDeviceType: .builtInWideAngleCamera, mediaType: AVMediaTypeVideo, position: .front) {
+                /*
+                 In some cases where users break their phones, the back wide angle camera is not available.
+                 In this case, we should default to the front wide angle camera.
+                 */
+                defaultVideoDevice = frontCameraDevice
+            }
+
+            let videoDeviceInput = try AVCaptureDeviceInput(device: defaultVideoDevice!)
+
+            if session.canAddInput(videoDeviceInput) {
+                session.addInput(videoDeviceInput)
+                self.deviceInput = videoDeviceInput
+
+                DispatchQueue.main.async {
+                    /*
+                     Why are we dispatching this to the main queue?
+                     Because AVCaptureVideoPreviewLayer is the backing layer for PreviewView and UIView
+                     can only be manipulated on the main thread.
+                     Note: As an exception to the above rule, it is not necessary to serialize video orientation changes
+                     on the AVCaptureVideoPreviewLayer’s connection with other session manipulation.
+
+                     Use the status bar orientation as the initial video orientation. Subsequent orientation changes are
+                     handled by CameraViewController.viewWillTransition(to:with:).
+                     */
+                    let statusBarOrientation = UIApplication.shared.statusBarOrientation
+                    var initialVideoOrientation: AVCaptureVideoOrientation = .portrait
+                    if statusBarOrientation != .unknown {
+                        if let videoOrientation = statusBarOrientation.videoOrientation {
+                            initialVideoOrientation = videoOrientation
+                        }
+                    }
+
+                    self.videoPreviewLayer.connection?.videoOrientation = initialVideoOrientation
+                }
+            } else {
+                print("Could not add video device input to the session")
+                setupResult = .configurationFailed
+                return
+            }
+        } catch {
+            print("Could not create video device input: \(error)")
+            setupResult = .configurationFailed
+            return
+        }
+
+        // Add audio input.
+        do {
+            let audioDevice = AVCaptureDevice.defaultDevice(withMediaType: AVMediaTypeAudio)
+            let audioDeviceInput = try AVCaptureDeviceInput(device: audioDevice!)
+
+            if session.canAddInput(audioDeviceInput) {
+                session.addInput(audioDeviceInput)
+            } else {
+                print("Could not add audio device input to the session")
+            }
+        } catch {
+            print("Could not create audio device input: \(error)")
+        }
+        
+        // Add photo output.
+        if session.canAddOutput(photoOutput) {
+            session.addOutput(photoOutput)
+
+            photoOutput.isHighResolutionCaptureEnabled = true
+            photoOutput.isLivePhotoCaptureEnabled = photoOutput.isLivePhotoCaptureSupported
+            //livePhotoMode = photoOutput.isLivePhotoCaptureSupported ? .on : .off
+        } else {
+            print("Could not add photo output to the session")
+            setupResult = .configurationFailed
+            return
+        }
+
+        // Add meta output.
+        if session.canAddOutput(metadataOutput) {
+            session.addOutput(metadataOutput)
+            metadataOutput.metadataObjectTypes = [AVMetadataObjectTypeQRCode, AVMetadataObjectTypeFace]
+            metadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+        } else {
+            print("Could not add meta output to the session")
+            setupResult = .configurationFailed
+            return
+        }
+    }
+
+    private func setupGestures() {
+        let doubleTapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(self.handleGestureCameraDoubleTap))
+        doubleTapGestureRecognizer.numberOfTapsRequired = 2
+        addGestureRecognizer(doubleTapGestureRecognizer)
+    }
+
+    private func setupObservers() {
+
+    }
+
+    private func removeObservers() {
+
+    }
+
+    // MARK: - Setup
+
+    public func requestAccess() {
+        /*
+         Check video authorization status. Video access is required and audio
+         access is optional. If audio access is denied, audio is not recorded
+         during movie recording.
+         */
+        switch AVCaptureDevice.authorizationStatus(forMediaType: AVMediaTypeVideo) {
+        case .authorized:
+            // The user has previously granted access to the camera.
+            break
+        case .notDetermined:
+            /*
+             The user has not yet been presented with the option to grant
+             video access. We suspend the session queue to delay session
+             setup until the access request has completed.
+
+             Note that audio access will be implicitly requested when we
+             create an AVCaptureDeviceInput for audio during session setup.
+             */
+            sessionQueue.suspend()
+            AVCaptureDevice.requestAccess(forMediaType: AVMediaTypeVideo, completionHandler: { [unowned self] granted in
+                if !granted {
+                    self.setupResult = .notAuthorized
+                }
+                self.sessionQueue.resume()
+            })
+
+        default:
+            // The user has previously denied access.
+            setupResult = .notAuthorized
+        }
+
+        /*
+         Setup the capture session.
+         In general it is not safe to mutate an AVCaptureSession or any of its
+         inputs, outputs, or connections from multiple threads at the same time.
+
+         Why not do all of this on the main queue?
+         Because AVCaptureSession.startRunning() is a blocking call which can
+         take a long time. We dispatch session setup to the sessionQueue so
+         that the main queue isn't blocked, which keeps the UI responsive.
+         */
+        sessionQueue.async { [unowned self] in
+            self.setupSession()
+        }
+    }
+
+    public func start() {
+        sessionQueue.async {
+            switch self.setupResult {
+            case .success:
+                // Only setup observers and start the session running if setup succeeded.
+                self.setupObservers()
+                self.session.startRunning()
+            case .notAuthorized:
+                DispatchQueue.main.async {
+                    print("Doesn't have permission to use the camera, please change privacy settings")
+                }
+            case .configurationFailed:
+                DispatchQueue.main.async {
+                    print("Unable to capture media")
+                }
+            }
+        }
+    }
+
+    public func stop() {
+        sessionQueue.async { [unowned self] in
+            if self.setupResult == .success {
+                self.session.stopRunning()
+                self.removeObservers()
+            }
+        }
+    }
+
+    // MARK: Actions
+
+    func handleGestureCameraDoubleTap(gesture: UITapGestureRecognizer) {
+
+    }
+
+}
+
+extension WSAugmentedCameraView: AVCaptureMetadataOutputObjectsDelegate {
+
+    public func captureOutput(_ captureOutput: AVCaptureOutput!, didOutputMetadataObjects metadataObjects: [Any]!, from connection: AVCaptureConnection!) {
+        for metadataObject in metadataObjects as! [AVMetadataObject] {
+            if metadataObject.type == AVMetadataObjectTypeFace {
+                let transformedMetadataObject = videoPreviewLayer.transformedMetadataObject(for: metadataObject)
+                print("We have a face", transformedMetadataObject?.bounds ?? CGRect.zero)
+            }
+        }
+    }
+    
+}
+
+extension UIDeviceOrientation {
+    var videoOrientation: AVCaptureVideoOrientation? {
+        switch self {
+        case .portrait: return .portrait
+        case .portraitUpsideDown: return .portraitUpsideDown
+        case .landscapeLeft: return .landscapeRight
+        case .landscapeRight: return .landscapeLeft
+        default: return nil
+        }
+    }
+}
+
+extension UIInterfaceOrientation {
+    var videoOrientation: AVCaptureVideoOrientation? {
+        switch self {
+        case .portrait: return .portrait
+        case .portraitUpsideDown: return .portraitUpsideDown
+        case .landscapeLeft: return .landscapeLeft
+        case .landscapeRight: return .landscapeRight
+        default: return nil
+        }
+    }
+}
--- WSAugmentedCameraExample/CameraViewController.swift
+++ WSAugmentedCameraExample/CameraViewController.swift
@@ -7,17 +7,45 @@
 //
 
 import UIKit
+import AVFoundation
 
 class CameraViewController: UIViewController {
 
+    let cameraView = WSAugmentedCameraView()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .green
+        setupUI()
+        cameraView.requestAccess()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        cameraView.start()
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        cameraView.stop()
+        super.viewWillDisappear(animated)
+    }
+
+    override var shouldAutorotate: Bool {
+        return true
+    }
+
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        return .all
+    }
+
+    func setupUI() {
+        view.addSubview(cameraView)
+        cameraView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            cameraView.topAnchor.constraint(equalTo: view.topAnchor),
+            cameraView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            cameraView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            cameraView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+        ])
     }
 
 }
